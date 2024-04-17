200003f49b92206ff58cda6ca80430b4f533bd73
BelExplorer <128513997+BelExplorer@users.noreply.github.com>
2024-04-17 12:41:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+/* OPEN/CLOSE MENU */
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", function() {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", function() {
+    nav.classList.remove("visible");
+})
+
+for (var i = 0; i < 3; i++) {
+    document.querySelectorAll(".nav-menu-item") [i].addEventListener("click", function() {
+        nav.classList.remove("visible");
+    })
+}
+
+document.querySelector("main").addEventListener("click", function() {
+    nav.classList.remove("visible");
+})
+
+
+/* SCROLL UP MENU */
+
+let ubicacionInicial = window.scrollY;
+window.onscroll = function() {
+    let desplazamientoActual = window.scrollY; 
+    if (ubicacionInicial >= desplazamientoActual) {
+        document.querySelector("#header").style.top = "0";
+    }
+    else {
+        document.querySelector("#header").style.top = "-100px";
+    }
+    ubicacionInicial = desplazamientoActual;
+}
